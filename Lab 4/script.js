61074219fc9eea02308229ca94f591554b36894b
Md. Shakil Hossain <shakiljusc15@gmail.com>
2021-12-11 12:48:15 +0600

    lab 4

---
+++ Lab 4/script.js
@@ -0,0 +1,91 @@
+
+// var a=5;
+// var b=4;
+// var name$ = "Md. Shakil Hossain";
+// let c = 7;
+
+
+// document.write(a+"<br/>");
+// console.log(typeof(a));
+
+// document.write(b+"<br/>");
+// console.log(typeof(b));
+
+// document.write(name$+"<br/>");
+// console.log(typeof(name$));
+
+
+// a = a.toString();
+// a = toString(a);
+// //document.write(a+"<br/>");
+// console.log(typeof(a));
+// console.log(a);
+
+
+
+// var name="shakil";
+// var age=21;
+// var age2=24;
+// var number=123.34;
+// document.writeln(name+" "+age+" "+number);
+// console.log(typeof(name));
+
+// console.log(typeof(age));
+
+// console.log(typeof(age2));
+
+// console.log(typeof(number));
+
+// age=toString(age);
+// console.log(typeof(age));
+
+// console.log(age2-age);
+
+// console.log(typeof(age2-age));
+
+
+
+// // user Input 
+
+// var studentFirstName = prompt ("Enter your frist name ");
+// var studentLastName = prompt ("Enter your Last name ");
+
+// document.writeln("String Concatention: " + studentFirstName);
+
+// var fname_input=prompt("Enter your first name");
+// var lname_input=prompt("Enter your last name");
+
+// document.write("Full name : "+fname_input.concat(" "+lname_input)+"<br>")
+
+
+
+
+// Task-1
+
+var a = prompt ("Enter your frist number :  ");
+var b = prompt ("Enter your frist number :  ");
+
+var x = parseInt(a);
+var y = parseInt(b);
+
+var c = (x+y);
+var d = a-b;
+var e = a*b;
+var f = a/b;
+var g = a%b;
+
+
+document.writeln(a+ "+" +b+ "=" +c+"<br/>");
+document.writeln(a+ "-" +b+ "=" +d+"<br/>");
+document.writeln(a+ "*" +b+ "=" +e+"<br/>");
+document.writeln(a+ "/" +b+ "=" +f+"<br/>");
+document.writeln(a+ "%" +b+ "=" +g+"<br/>");
+
+
+// Task-2
+
+var a1 = prompt ("Enter value of radius of a circle :  ");
+var a2 = parseInt(a1);
+var a3 = 3.1416 *a2*a2;
+document.writeln("The area of a circle : "+a3+"<br/>");
+
