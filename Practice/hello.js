cbd570cd5f6baed9fac0f04c205947d86fd784fd
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-01-19 12:37:28 +0600

    Practices

---
+++ Practice/hello.js
@@ -0,0 +1,13 @@
+console.log('Assalamualaikum');
+console.log(10);
+
+$(document).ready(function(){
+// typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student" , "Programmer", "Web Developer", "Blogger", "Graphic Designer",  "Ethical Hacker"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
---
+++ Practice/variable.js
@@ -0,0 +1,47 @@
+// var variable name = value;
+
+// Number
+
+var vname = 12;
+console.log(vname);
+console.log(typeof vname);
+
+var num = '20.5';
+num = parseFloat(num);   //num = +num;
+console.log(num);
+
+vname = ''+vname;
+console.log(typeof vname);
+
+var num2 = 30.3;
+console.log(num2.toFixed(5));
+
+
+
+
+// String 
+
+var name = "My name is Md. Shakil Hossain";
+console.log(name);
+console.log(typeof name);
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.indexOf('is'));
+console.log(name.split(' '));
+console.log(name.split('i'));
+
+
+
+// Boolean
+
+var isHot = true;
+var isRich = false;
+console.log (isHot);
+console.log(isRich);
+console.log(typeof isHot);
+console.log(typeof isRich);
+
+
+
+
+
