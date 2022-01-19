a12fa82415b4e9bc0d189b79560857029d8d5e06
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-01-19 12:35:11 +0600

    Lab 11

---
+++ Lab 11/ninth.js
@@ -0,0 +1,14 @@
+// document.querySelector("button").addEventListener("click", listenerFunction);
+
+//  function listenerFunction(){
+//     alert("Hell0");
+//  }
+
+
+//  document.querySelector("button").addEventListener("mouseover", function listenerFunction(a, b){
+//     alert(a+b);
+//  });
+
+document.querySelector("button").addEventListener("mouseover", function(a, b){
+    document.write("Hell0");
+ });
